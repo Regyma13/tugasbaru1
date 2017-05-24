96ff70b5a363b6470cf83aa6eddd7ca5136119aa
regyma <regymahmudazis@gmail.com>
2017-05-24 22:58:54 +0700

    tambah method cariLuasLingkaran

--- HitungLuas.java
+++ HitungLuas.java
@@ -4,6 +4,10 @@ class HitungLuas {
 		return panjang * lebar;	
 	}
 
+	public static float cariLuasLingkaran(int jarijari) {
+		return (22/7) * jarijari * jarijari;
+	}
+
 	public static void main (String[] args) {
 
 	}
